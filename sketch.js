ef0d3af0041b9e0d8cff6545e6b90442d076d456
AKASH RAJ <akashraj9828@gmail.com>
2017-09-19 16:31:43 +0530

    dev

--- sketch.js
+++ sketch.js
@@ -1,51 +1,56 @@
 
-var scl=40;
+var scl = 40;
 var food;
 
 function setup() {
-createCanvas(400,400);
-s= new snake();
-frameRate(7);
-	
-pickLocation();
+
+	canvas = createCanvas(400, 400);
+	canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2)
+	s = new snake();
+	frameRate(7);
+
+	pickLocation();
 }
 
 function draw() {
-  background(51);
-  s.update();
-  s.show();
+	background(51);
 
-  
-  if (s.eat(food))
-  {
-	  pickLocation();
-	  
-  }
-  
-  
-  
-	fill(255,0,100);
-  rect(food.x,food.y,scl,scl);
-  
-  
+	renderSnake()
+	renderFood()
+
+
+
 }
 
-function pickLocation(){
-	var cols=floor(width/scl);
-	var rows=floor(height/scl);
-	food=createVector(floor(random(cols)),floor(random(rows)));
-	food.mult(scl);	
+function renderSnake() {
+	s.update();
+	s.show();
+}
+
+function renderFood()){
+	if (s.eat(food)) {
+		pickLocation();
+	}
+	fill(255, 0, 100);
+	rect(food.x, food.y, scl, scl);
+}
+
+function pickLocation() {
+	var cols = floor(width / scl);
+	var rows = floor(height / scl);
+	food = createVector(floor(random(cols)), floor(random(rows)));
+	food.mult(scl);
 }
 
 
-function keyPressed(){
-	if(keyCode===UP_ARROW){
-		s.dir(0,-1)
-	}else if(keyCode===DOWN_ARROW){
-		s.dir(0,1)
-	}else if(keyCode===RIGHT_ARROW){
-		s.dir(1,0)
-	}else if(keyCode===LEFT_ARROW){
-		s.dir(-1,0)
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		s.dir(0, -1)
+	} else if (keyCode === DOWN_ARROW) {
+		s.dir(0, 1)
+	} else if (keyCode === RIGHT_ARROW) {
+		s.dir(1, 0)
+	} else if (keyCode === LEFT_ARROW) {
+		s.dir(-1, 0)
 	}
 }
